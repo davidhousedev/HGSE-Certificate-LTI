223f24d49785c5c1cd80e0e5aa8eb55559254b9a
David House <davidhousedev@gmail.com>
2016-05-13 16:15:27 -0400

    Renamed files

--- public/ppe_online_cert.js
+++ public/ppe_online_cert.js
@@ -1,38 +1,4 @@
-/**
- * This function is implemented in order to allow text to be centered in a PDF.
- * The feature is implemented by jsPDF but is not currently working as of 5/5/2016.
- */
-    (function(API){
-    API.myText = function(txt, options, x, y) {
-        options = options ||{};
-        /* Use the options align property to specify desired text alignment
-         * Param x will be ignored if desired text alignment is 'center'.
-         * Usage of options can easily extend the function to apply different text 
-         * styles and sizes 
-        */
-        if( options.align == "center" ){
-            // Get current font size
-            var fontSize = this.internal.getFontSize();
 
-            // Get page width
-            var pageWidth = this.internal.pageSize.width;
-
-            // Get the actual text's width
-            /* You multiply the unit width of your string by your font size and divide
-             * by the internal scale factor. The division is necessary
-             * for the case where you use units other than 'pt' in the constructor
-             * of jsPDF.
-            */
-            txtWidth = this.getStringUnitWidth(txt)*fontSize/this.internal.scaleFactor;
-
-            // Calculate text's x coordinate
-            x = ( pageWidth - txtWidth ) / 2;
-        }
-
-        // Draw text at x,y
-        this.text(txt,x,y);
-    }
-})(jsPDF.API);
 
 /**
  * Primary PDF creation function
@@ -135,21 +101,7 @@ doc.save('ppe_cert.pdf')
 /**
  * Implemented for testing
  */
- /*
-function testRuby(person, course){
 
-        var thing = new jsPDF();
-        thing.myText(person.fullName, {align: "center"},0,100); // prints centered text according to custom function
-        thing.myText(course.courseName, {align: "center"},0,200); // prints centered text according to custom function
-
-        thing.save('Test.pdf');
-
-    };
-
-$('#rubyTest').click(function(){
-    testRuby("fjsoeijf");
-});
-*/
 
 $('#test').click(function(){
     alert("Hello, jQuery");
